54b128fe519594c4e7aba19c10678f32e45ed733
Rosenboum <RosenZM@yandex.ru>
2020-01-27 16:16:35 +0700

    Initial commit

---
+++ src/main/java/ru/hubsmc/hubslobby/HubsLobby.java
@@ -0,0 +1,38 @@
+package ru.hubsmc.hubslobby;
+
+import ru.hubsmc.hubscore.HubsPlugin;
+import ru.hubsmc.hubscore.HubsServer;
+
+public final class HubsLobby extends HubsPlugin implements HubsServer {
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onPlayerJoin() {
+
+    }
+
+    @Override
+    public void onPlayerQuit() {
+
+    }
+
+}
